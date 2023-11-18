3831c8d5b2782d0c798e178aa3bfe3e4de0ebf72
cotyounoyume <cotyounoyume@gmail.com>
2023-11-18 13:27:56 +0900

    success lookById

--- Plugin/src/Event.cpp
+++ Plugin/src/Event.cpp
@@ -12,9 +12,16 @@ namespace Events
 		//for future release
 		if (a_event.menuName == "PauseMenu" and a_event.opening) {
 			Config::ReadIni();
+			SetupForms();
 		}
 		if (a_event.menuName == "LoadingMenu" and a_event.opening) {
 			NeedReset = true;
+		}
+		if (a_event.menuName == "DialogueMenu" and a_event.opening) {
+			DialogueTarget = RE::PlayerCharacter::GetSingleton()->crosshairRef;
+		}
+		if (a_event.menuName == "DialogueMenu" and a_event.opening == false) {
+			DialogueTarget = nullptr;
 		}
 		return RE::BSEventNotifyControl::kContinue;
 	}
@@ -23,5 +30,33 @@ namespace Events
 	{
 		//for future release
 		return RE::BSEventNotifyControl::kContinue;
+	}
+
+	void SetupForms()
+	{
+		RE::TESForm* form = RE::TESForm::LookupByID(0x6dead);
+		//auto         id = RE::PlayerCharacter::GetSingleton()->GetFormID();
+		//RE::TESForm* form = RE::TESForm::LookupByID(id);
+		if (!form) {
+			Utility::Notification(fmt::format("form not found."));
+		} else {
+			Utility::Notification(fmt::format("form:{} is {}", Utility::num2hex(form->GetFormID()), form->GetFormEditorID()));
+		}
+
+		RE::TESForm* form2 = RE::TESForm::LookupByEditorID("DontStripThis");
+		//auto         id = RE::PlayerCharacter::GetSingleton()->GetFormID();
+		//RE::TESForm* form = RE::TESForm::LookupByID(id);
+		if (!form) {
+			Utility::Notification(fmt::format("form2 not found."));
+		} else {
+			Utility::Notification(fmt::format("form2:{} is {}", Utility::num2hex(form->GetFormID()), form->GetFormEditorID()));
+			if (form2->Is<RE::BGSKeyword>()) {
+				Utility::Notification(fmt::format("form2:{} is {}, and This is BGSKeyword", Utility::num2hex(form2->GetFormID()), form2->GetFormEditorID()));
+				auto form3 = dynamic_cast<RE::BGSKeyword*>(form2);
+				if (form3) {
+					Utility::Notification(fmt::format("form3:{}", form3->GetFormEditorID()));
+				}
+			}
+		}
 	}
 }
